446b72f499513e4e7ec485e102c04142bd3b63be
ShuffleZZZ <mihpihnaty@yandex.ru>
2020-06-09 17:14:36 +0300

    add convergence check for Jacobi

--- hw2/Jacobi.py
+++ hw2/Jacobi.py
@@ -5,7 +5,14 @@ def jacobi(A, b, eps, maxIterations):
   x = np.zeros(size)
 
   D = np.diag(A)
-  R = A - np.diagflat(D)
+  Dm = np.diagflat(D)
+  Dneg = np.diagflat([1 / n for n in D])
+  R = A - Dm
+
+  w, v = np.linalg.eig(np.dot(Dneg, R))
+  if max(abs(w)) > 1:
+    print("Spectral radius of matrix is less than 1, aborting calculation")
+    return
 
   for i in range(size):
     if sum(abs(R[i])) >= abs(D[i]):
