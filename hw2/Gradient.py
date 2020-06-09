821f7d29c8c0aa5149b54e85c27cd62af98a3888
RevealMind <99_dimas@mail.ru>
2020-06-10 02:49:48 +0300

    add extra criteria in Gradient

--- hw2/Gradient.py
+++ hw2/Gradient.py
@@ -2,22 +2,23 @@ import numpy as np
 
 def gradient(A, b, eps, maxIterations):
   m = len(A)
-  xk = np.random.rand(m)
+  xk = np.zeros(m)
   rk = b - np.dot(A, xk)
   zk = rk
 
   for k in range(maxIterations):
-    print(f"{k}-th iteration:")
-    print(xk)
-    if np.linalg.norm(np.dot(A, xk) - b) <= eps * np.linalg.norm(b):
-      print("^^^^^^^^^^^^^^")
-      print("Solution found")
-      return
     a = np.dot(rk, rk) / np.dot(np.dot(A, zk), zk)
     x = xk + a * zk
     r = rk - a * np.dot(A, zk)
     beta = np.dot(r, r) / np.dot(rk, rk)
     z = r + zk * beta
+    print(f"{k}-th iteration:")
+    print(xk)
+    if (np.linalg.norm(np.dot(A, xk) - b) <= eps * np.linalg.norm(b)
+          or np.linalg.norm(x - xk) <= eps * np.linalg.norm(xk)):
+      print("^^^^^^^^^^^^^^")
+      print("Solution found")
+      return
     xk, rk, zk = x, r, z
 
   print("Number of iterations exceeded")
